26efed1c274103714db3de80009e85e515df91b4
Niyaz Bin Hashem <niyazed@outlook.com>
2020-06-07 13:16:39 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 '''
 Multithreaded video processing sample.
 Usage:
-   run.py {<video device number> | default set to 0}
+   run.py {<video device number> | <rtsp stream>}
 
    Shows how python threading capabilities can be used
    to organize parallel captured frame processing pipeline
@@ -18,21 +18,29 @@ import argparse
 import os
 import cv2
 import imutils
+import sys 
+
+import logging
+logging.basicConfig(level=logging.NOTSET)
 
 from VideoGet import VideoGet
 from VideoShow import VideoShow
 import multiprocessing as mp
 
 
-def threadBoth(source=0):
+logger=logging.getLogger("videoapp") 
+
+
+def threadBoth(source):
     """
     Dedicated thread for grabbing video frames with VideoGet object.
     Dedicated thread for showing video frames with VideoShow object.
     Main thread serves only to pass frames between VideoGet and
     VideoShow objects/threads.
     """
-
+    
     video_getter = VideoGet(source).start()
+    logger.info("Capturing Frame...")
     video_shower = VideoShow(video_getter.frame).start()
 
     while True:
@@ -41,6 +49,7 @@ def threadBoth(source=0):
             video_getter.stop()
             break
 
+        
         frame = video_getter.frame
         frame = imutils.resize(frame, width=1080)
         video_shower.frame = frame
@@ -64,10 +73,15 @@ def main():
 
 
     # Paralle the execution of a function across multiple input values
-    num_processes = mp.cpu_count()
-    p = mp.Pool(num_processes)
-    p.map(threadBoth(args["source"]), range(num_processes))
-    
+    try:
+        num_processes = mp.cpu_count()
+        logger.info("No. of Cores: {}".format(num_processes))
+        p = mp.Pool(num_processes)
+        p.map(threadBoth(args["source"]), range(num_processes))
+    except:
+        pass
+        
+
 
 if __name__ == "__main__":
     print(__doc__)
